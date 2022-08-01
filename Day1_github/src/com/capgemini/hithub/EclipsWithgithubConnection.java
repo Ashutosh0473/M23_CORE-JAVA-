85d01c2328528dbd77e443344355f99a9b9c226b
Ashu <Ashu@LAPTOP-T6Q9DMMU>
2022-08-01 15:47:18 +0530

    first commit

---
+++ Day1_github/src/com/capgemini/hithub/EclipsWithgithubConnection.java
@@ -0,0 +1,10 @@
+package com.capgemini.hithub;
+//demo for connect eclips with github
+public class EclipsWithgithubConnection {
+
+	public static void main(String[] args) {
+		
+System.out.println("welcome to capgemini c2tc program");
+	}
+
+}
