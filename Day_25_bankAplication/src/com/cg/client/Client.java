7477765eab2622def0d68f4a9a7105fe204ad463
Ashu <Ashu@LAPTOP-T6Q9DMMU>
2022-09-20 09:21:55 +0530

    first commit

---
+++ Day_25_bankAplication/src/com/cg/client/Client.java
@@ -0,0 +1,30 @@
+package com.cg.client;
+
+import com.cg.application.MMCurrentAcc;
+import com.cg.application.MMSavingAcc;
+
+import com.cg.framework.CurrentAcc;
+import com.cg.framework.SavingAcc;
+
+public class Client 
+{
+	public static void main(String[] args)
+	{
+	
+	//here minimum account balance limit is 500 
+	SavingAcc   ms= new MMSavingAcc(1001,"Ashutosh Sawant",800, true);
+	
+	//current limit is 2500 
+	CurrentAcc mc= new MMCurrentAcc(1002,"Rakesh jadhav",5000,2500);
+	
+	
+	System.out.println("Saving Account : ");
+	ms.withdraw(100);
+	
+	System.out.println("Current Account : ");
+	mc.withdraw(3000);
+	
+	
+	}
+	
+}
